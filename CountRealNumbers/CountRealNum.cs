df3de8a61e10f97fdc4fda9c3079c0b350457fd5
Deyan <dlucky95@gmail.com>
2017-02-19 23:38:09 +0200

    initial commit

---
+++ CountRealNumbers/CountRealNum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+    class CountRealNum
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var num in input)
+            {
+                double parsedNum = double.Parse(num);
+
+                if(counts.ContainsKey(parsedNum))
+                {
+                    counts[parsedNum]++;
+                }
+                else
+                {
+                    counts[parsedNum] = 1;
+                }
+            }
+
+            foreach (var num in counts.Keys)
+            {
+                Console.WriteLine($"{num} -> {counts[num]}");
+            }
+        }
+    }
+}
---
+++ WordCount-Example/WordCount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount_Example
+{
+    class WordCount
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().ToLower().Split().ToArray();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if(counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts[word] = 1;
+                }
+            }
+
+            foreach (var entry in counts)
+            {
+                if(entry.Value % 2 != 0)
+                {
+                    Console.WriteLine(entry.Key);
+                }
+            }
+
+        }
+    }
+}
