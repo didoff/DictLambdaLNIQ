cd1fb1b5b89743b18897cc90ec327c863894cd53
Deyan <dlucky95@gmail.com>
2017-02-24 21:48:04 +0200

    Largest 3

---
+++ Largest3/Largest3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest3
+{
+    class Largest3
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().ToList();
+            List<int> intNum = new List<int>();
+
+            foreach (var num in input)
+            {
+                intNum.Add(Int32.Parse(num));
+            }
+
+            intNum = intNum.OrderByDescending(x => x)
+                .Take(3)
+                .ToList();
+
+            foreach (var num in intNum)
+            {
+                Console.Write(num);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
