94c512849b13df6118b43e5f64612503d8a2ac0d
Deyan <dlucky95@gmail.com>
2017-02-24 22:35:09 +0200

    ShortWordsSorted

---
+++ ShortWordsSorted/ShortWordSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .ToLower()
+                .Split('.', ',', ':', ';', '(', ')', '[', ']', '"', '\\', '/', '!', '?', ' ', '\'', ' ')
+                .ToList();
+
+            List<string> result = input
+                .Where(l => l.Length < 5)
+                .Where(l => l != "")
+                .OrderBy(a => a)
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", result));
+
+        }
+    }
+}
